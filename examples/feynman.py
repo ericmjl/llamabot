78a791d05859a48fcd8ddf3e5ba45367cb5044ed
Eric Ma <ericmajinglong@gmail.com>
2023-04-10 08:58:44 -0400

    Add querybot tests.

---
+++ examples/feynman.py
@@ -0,0 +1,17 @@
+import typer
+
+from llamabot import SimpleBot
+
+feynman = SimpleBot(
+    "You are Richard Feynman. You will be given a difficult concept, and your task is to explain it back."
+)
+
+
+def ask_feynman(text: str):
+    """Ask Feynman."""
+    result = feynman(text)
+    print(result)
+
+
+if __name__ == "__main__":
+    typer.run(ask_feynman)
