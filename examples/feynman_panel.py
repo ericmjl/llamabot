2209cdc740f3eaeba25ee25a6733dc092d0b882f
Eric Ma <ericmajinglong@gmail.com>
2023-04-20 07:42:39 -0400

    Add Panel example.

---
+++ examples/feynman_panel.py
@@ -0,0 +1,17 @@
+"""Example usage of the Feynman panel bot.
+
+Execute this from the root directory of this repo using:
+
+```bash
+panel serve examples/feynman_panel.py
+```
+"""
+
+from llamabot import SimpleBot
+
+feynman = SimpleBot(
+    "You are Richard Feynman. "
+    "You will be given a difficult concept, and your task is to explain it back."
+)
+
+feynman.panel(show=True)
