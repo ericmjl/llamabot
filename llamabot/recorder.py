18ae00716978c0a39ebcab68d343c092ce119017
Eric Ma <ericmajinglong@gmail.com>
2023-05-03 23:07:19 -0400

    Add panel representation of a recorder class.

--- llamabot/recorder.py
+++ llamabot/recorder.py
@@ -3,6 +3,7 @@ import contextvars
 from typing import Optional
 
 import pandas as pd
+import panel as pn
 
 prompt_recorder_var = contextvars.ContextVar("prompt_recorder")
 
@@ -52,7 +53,79 @@ class PromptRecorder:
 
         :return: We delegate to the _repr_html_ method of the pandas DataFrame class.
         """
-        return pd.DataFrame(self.prompts_and_responses)._repr_html_()
+        return self.dataframe()._repr_html_()
+
+    def dataframe(self):
+        """Return a pandas DataFrame representation of the prompt recorder.
+
+        :return: A pandas DataFrame representation of the prompt recorder.
+        """
+        return pd.DataFrame(self.prompts_and_responses)
+
+    def panel(self):
+        """Return a panel representation of the prompt recorder.
+
+        :return: A panel representation of the prompt recorder.
+        """
+        global index
+        index = 0
+        pn.extension()
+
+        next_button = pn.widgets.Button(name=">")
+        next_button
+        prev_button = pn.widgets.Button(name="<")
+        prev_button
+
+        buttons = pn.Row(prev_button, next_button)
+
+        prompt_header = pn.pane.Markdown("# Prompt")
+        prompt_display = pn.pane.Markdown(self.prompts_and_responses[index]["prompt"])
+
+        prompt = pn.Column(prompt_header, prompt_display)
+
+        response_header = pn.pane.Markdown("# Response")
+        response_display = pn.pane.Markdown(
+            self.prompts_and_responses[index]["response"]
+        )
+        response = pn.Column(response_header, response_display)
+
+        display = pn.Row(prompt, response)
+
+        def update_objects(index):
+            """Update the prompt and response Markdown panes.
+
+            :param index: The index of the prompt and response to update.
+            """
+            prompt_display.object = self.prompts_and_responses[index]["prompt"]
+            response_display.object = self.prompts_and_responses[index]["response"]
+
+        def next_button_callback(event):
+            """Callback function for the next button.
+
+            :param event: The click event.
+            """
+            global index
+            index += 1
+            if index > len(self.prompts_and_responses) - 1:
+                index = len(self.prompts_and_responses) - 1
+
+            update_objects(index)
+
+        def prev_button_callback(event):
+            """Callback function for the previous button.
+
+            :param event: The click event.
+            """
+            global index
+            index -= 1
+            if index < 0:
+                index = 0
+            update_objects(index)
+
+        next_button.on_click(next_button_callback)
+        prev_button.on_click(prev_button_callback)
+
+        return pn.Column(buttons, display)
 
 
 def autorecord(prompt: str, response: str):
