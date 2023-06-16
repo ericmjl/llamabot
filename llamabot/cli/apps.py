0920e4c23726ace5f09f9961e800344b4d847035
Eric Ma <ericmajinglong@gmail.com>
2023-06-15 21:39:19 -0400

    Made port and address configurable.

--- llamabot/cli/apps.py
+++ llamabot/cli/apps.py
@@ -1,4 +1,6 @@
 """Run llamabot codebot panel apps."""
+import os
+
 import typer
 
 from llamabot.bot_library import coding
@@ -7,11 +9,16 @@ app = typer.Typer()
 
 
 @app.command()
-def codebot():
-    """Run the codebot app."""
+def codebot(port: int = 5050, address: str = "0.0.0.0"):
+    """Run the codebot app.
+
+    :param port: Port to run the app on.
+    :param address: Address to run the app on.
+    """
 
     codebot_app = coding.create_panel_app()
-    codebot_app.show()
+    os.environ["BOKEH_ALLOW_WS_ORIGIN"] = f"{address}:{port}"
+    codebot_app.show(title="Codebot", port=port, address=address)
 
 
 if __name__ == "__main__":
