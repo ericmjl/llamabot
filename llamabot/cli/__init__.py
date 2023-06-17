ef812f8d34a294503e34f87e8164f45a90711483
Eric Ma <ericmajinglong@gmail.com>
2023-06-17 10:34:45 -0400

    Update module-level docstring for CLI init.

--- llamabot/cli/__init__.py
+++ llamabot/cli/__init__.py
@@ -1,10 +1,29 @@
-"""Top-level command line interface."""
+"""
+This module provides a top-level command line interface for interacting with apps and python.
+
+Classes:
+    - None
+
+Exceptions:
+    - None
+
+Functions:
+    - None
+
+Objects:
+    - app: A Typer instance for managing command line interface.
+
+Modules:
+    - apps: A module for managing apps-related commands.
+    - python: A module for managing python-related commands.
+"""
 import typer
 
-from . import apps
+from . import apps, python
 
 app = typer.Typer()
 app.add_typer(apps.app, name="apps")
+app.add_typer(python.app, name="python")
 
 if __name__ == "__main__":
     app()
