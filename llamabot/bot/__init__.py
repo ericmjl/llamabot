d1c50812a2b7823a2d03141199fa2e3bda140696
Eric Ma <ericmajinglong@gmail.com>
2023-06-17 20:01:47 -0400

    Add more explicit validation checks on the presence of the openai API key.

--- llamabot/bot/__init__.py
+++ llamabot/bot/__init__.py
@@ -18,7 +18,10 @@ config_path = Path.home() / ".llamabotrc"
 if config_path.exists():
     load_dotenv(config_path)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY", None)
+if api_key is None:
+    raise ValueError("OPENAI_API_KEY environment variable must be set.")
+openai.api_key = api_key
 
 
 __all__ = ["SimpleBot", "ChatBot", "QueryBot"]
