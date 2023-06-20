ca9b1c82d368d45c3a647e92727b8218c36a4bb7
Eric Ma <ericmajinglong@gmail.com>
2023-06-19 23:11:25 -0400

    docs: Add Conventional Commits specification explanation
    This commit adds a detailed explanation of the Conventional Commits
    specification to the git.py file. It outlines the various commit types,
    scopes, and footers, as well as their correlation with Semantic
    Versioning. This information will help users understand the importance
    of following the Conventional Commits specification when crafting their
    commit messages.

--- llamabot/prompt_library/git.py
+++ llamabot/prompt_library/git.py
@@ -18,5 +18,38 @@ def write_commit_message(diff: str):
 
     {{ diff }}
 
+    Use the Conventional Commits specification.
+
+    ```
+    <type>[optional scope]: <description>
+
+    [optional body]
+
+    [optional footer(s)]
+    ```
+
+    The commit contains the following structural elements, to communicate intent to the consumers of your library:
+
+    fix: a commit of the type fix patches a bug in your codebase
+        (this correlates with PATCH in Semantic Versioning).
+    feat: a commit of the type feat introduces a new feature to the codebase
+        (this correlates with MINOR in Semantic Versioning).
+    BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope,
+        introduces a breaking API change (correlating with MAJOR in Semantic Versioning).
+        A BREAKING CHANGE can be part of commits of any type.
+
+    types other than fix: and feat: are allowed,
+    for example @commitlint/config-conventional (based on the Angular convention) recommends
+    build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
+
+    footers other than BREAKING CHANGE: <description> may be provided
+    and follow a convention similar to git trailer format.
+
+    Additional types are not mandated by the Conventional Commits specification,
+    and have no implicit effect in Semantic Versioning (unless they include a BREAKING CHANGE).
+    A scope may be provided to a commit's type,
+    to provide additional contextual information and is contained within parenthesis,
+    e.g., feat(parser): add ability to parse arrays.
+
     # noqa: DAR101
     """
