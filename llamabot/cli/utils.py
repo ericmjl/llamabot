b32986dcc6d8cb3ede37765d3f7a8cf63406b395
Eric Ma <ericmajinglong@gmail.com>
2023-06-21 08:43:22 -0400

    refactor(cli/utils): enhance get_valid_input function
    - Add valid_inputs parameter to get_valid_input function
    - Update input prompt to include valid_inputs
    - Update input validation to check against valid_inputs
    - Update error message to display valid_inputs options

--- llamabot/cli/utils.py
+++ llamabot/cli/utils.py
@@ -1,7 +1,7 @@
 """CLI utility functions."""
 
 
-def get_valid_input(prompt):
+def get_valid_input(prompt, valid_inputs=("y", "n")):
     """
     This function prompts the user for input and validates it.
 
@@ -10,11 +10,13 @@ def get_valid_input(prompt):
         user_choice = get_valid_input("Enter 'y' for yes or 'n' for no: ")
 
     :param prompt: The prompt to display to the user.
+    :param valid_inputs: A tuple of valid inputs.
     :return: The validated user input, either 'y' or 'n'.
     """
+    msg = f"{prompt} {valid_inputs}"
     while True:
-        user_input = input(prompt).lower()
-        if user_input == "y" or user_input == "n":
+        user_input = input(msg).lower()
+        if user_input in valid_inputs:
             return user_input
         else:
-            print("Invalid input. Please enter 'y' or 'n'.")
+            print(f"Invalid input. Please enter one of { '/'.join(valid_inputs) }.")
