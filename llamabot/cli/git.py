37baea7726e92bb6a4d50758a8561cfe8b1dba92
Eric Ma <ericmajinglong@gmail.com>
2023-06-21 08:42:50 -0400

    feat(cli): add manual commit message editing option
    Allow users to manually edit the generated commit message using their
    system's default text editor. This is done by creating a temporary file
    with the generated message, opening it in the editor, and reading the
    edited message back into the script. The 'm' option is added to the
    user input prompt to trigger manual editing.

--- llamabot/cli/git.py
+++ llamabot/cli/git.py
@@ -1,7 +1,9 @@
 """Git subcommand for LlamaBot CLI."""
 
+import os
+from tempfile import NamedTemporaryFile
+
 import git
-import pyperclip
 from typer import Typer
 
 from llamabot.code_manipulation import get_git_diff
@@ -19,7 +21,6 @@ def commit(autocommit: bool = True):
     :param autocommit: Whether to automatically commit the changes.
     """
     repo = git.Repo(search_parent_directories=True)
-
     bot = commitbot()
 
     while True:
@@ -31,11 +32,26 @@ def commit(autocommit: bool = True):
             return
         message = bot(write_commit_message(diff))
         print("\n\n")
-        user_response = get_valid_input("Do you accept this commit message? (y/n) ")
+        user_response = get_valid_input(
+            "Do you accept this commit message?", ("y", "n", "m")
+        )
         if user_response == "y":
             break
+        elif user_response == "m":
+            # Provide option to edit the commit message
+            with NamedTemporaryFile(mode="w+", delete=False) as temp_file:
+                temp_file.write(message.content)
+                temp_file.flush()
+                editor = os.getenv(
+                    "EDITOR", "nano"
+                )  # Use the system's default text editor
+                os.system(f"{editor} {temp_file.name}")
+                temp_file.seek(0)
+                edited_message = temp_file.read().strip()
+                message.content = edited_message
+                break
+
     if autocommit:
         repo.index.commit(message.content)
         origin = repo.remote(name="origin")
         origin.push()
-    pyperclip.copy(message.content)
