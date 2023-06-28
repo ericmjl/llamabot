b1ccf35061babe39734abb4812d4e5b68d6515bc
Eric Ma <ericmajinglong@gmail.com>
2023-06-27 21:55:03 -0400

    feat(zotero): add sync command to sync Zotero items to local JSON file
    - Added import statements for json, os, pathlib, dotenv, pyzotero, and rich.progress
    - Loaded environment variables using dotenv
    - Created ZOTERO_JSON_PATH constant and ensured parent directory exists
    - Implemented sync command to fetch Zotero items and save them to a local JSON file

--- llamabot/cli/zotero.py
+++ llamabot/cli/zotero.py
@@ -1,9 +1,21 @@
 """Llamabot Zotero CLI."""
+import json
+import os
+from pathlib import Path
+
 import typer
+from dotenv import load_dotenv
+from pyzotero import zotero
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
 from .utils import configure_environment_variable
 
+load_dotenv()
+
 app = typer.Typer()
+
+ZOTERO_JSON_PATH = Path.home() / ".llamabot/zotero/zotero_index.json"
+ZOTERO_JSON_PATH.parent.mkdir(parents=True, exist_ok=True)
 
 
 @app.command()
@@ -23,9 +35,31 @@ def configure(
     configure_environment_variable("ZOTERO_LIBRARY_TYPE", library_type)
 
 
-# @app.command()
-# def sync():
-#     pass
+@app.command()
+def sync():
+    """Sync Zotero items to a local JSON file."""
+    zotero_library_id = os.environ.get("ZOTERO_LIBRARY_ID", None)
+    zotero_library_type = os.environ.get("ZOTERO_LIBRARY_TYPE", None)
+    zotero_api_key = os.environ.get("ZOTERO_API_KEY", None)
+
+    zot = zotero.Zotero(
+        library_id=zotero_library_id,
+        library_type=zotero_library_type,
+        api_key=zotero_api_key,
+    )
+
+    progress = Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    )
+    with progress:
+        progress.add_task("Syncing Zotero items...")
+        items = zot.everything(zot.items())
+
+    with open(ZOTERO_JSON_PATH, "w+") as f:
+        for item in items:
+            f.write(json.dumps(item) + "\n")
 
 
 # @app.command()
