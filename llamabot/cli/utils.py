3ba93ef6ef2b1ce91ccaf0bb4196198c8db9b5de
Eric Ma <ericmajinglong@gmail.com>
2023-06-27 21:16:34 -0400

    feat(cli/utils): add environment variable configuration
    - Add `configure_environment_variable` function to handle environment variable configuration in ~/.llamabot/.llamabotrc
    - Import `Path` from `pathlib` for file handling

--- llamabot/cli/utils.py
+++ llamabot/cli/utils.py
@@ -1,5 +1,7 @@
 """CLI utility functions."""
 
+from pathlib import Path
+
 
 def get_valid_input(prompt, valid_inputs=("y", "n")):
     """
@@ -20,3 +22,35 @@ def get_valid_input(prompt, valid_inputs=("y", "n")):
             return user_input
         else:
             print(f"Invalid input. Please enter one of { '/'.join(valid_inputs) }.")
+
+
+def configure_environment_variable(env_var: str, env_value: str):
+    """Configure environment variable in ~/.llamabotrc.
+
+    :param env_var: The name of the environment variable.
+        Will be converted to upper case.
+    :param env_value: The API key.
+    """
+    config_file = Path.home() / ".llamabot/.llamabotrc"
+    env_var_line = f'export {env_var.upper()}="{env_value}"'
+
+    if config_file.exists():
+        with open(config_file, "r") as file:
+            content = file.readlines()
+
+        found_env_var = False
+        with open(config_file, "w") as file:
+            for line in content:
+                if f"export {env_var.upper()}=" in line:
+                    print(f"found env var line {env_var_line}")
+                    file.write(env_var_line + "\n")
+                    found_env_var = True
+                else:
+                    file.write(line)
+
+        if not found_env_var:
+            with open(config_file, "a") as file:
+                file.write(env_var_line + "\n")
+    else:
+        with open(config_file, "w") as file:
+            file.write(env_var_line + "\n")
