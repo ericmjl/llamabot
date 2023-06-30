a813c5ac1f81dfa6ed845048d82361a2be64f068
Eric Ma <ericmajinglong@gmail.com>
2023-06-30 18:14:29 -0400

    feat(zotero): enhance Zotero library and item classes
    - Added `field` import from `dataclasses` and `Optional` import from `typing` to `library.py`
    - Refactored `ZoteroLibrary` class to use `field` for `library_path` and changed `library` to a dictionary for faster lookup
    - Added `keys` method to `ZoteroLibrary` class to return all keys from the library
    - Added `Optional` type hint to `library` attribute in `ZoteroItem` class
    - Enhanced `__getitem__` method in `ZoteroItem` class to raise `KeyError` if key is not found
    - Added `has_pdf`, `get` and `pdf` methods to `ZoteroItem` class for better PDF handling
    - Refactored `download_pdf` method in `ZoteroItem` class to check if item has a PDF before attempting to download
    
    This commit improves the functionality and usability of the Zotero library and item classes.

--- llamabot/zotero/library.py
+++ llamabot/zotero/library.py
@@ -1,7 +1,8 @@
 """Zotero library wrappers."""
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Optional
 
 from .utils import load_zotero
 
@@ -10,11 +11,17 @@ from .utils import load_zotero
 class ZoteroLibrary:
     """Zotero library."""
 
-    library_path: Path = Path.home() / ".llamabot/zotero/zotero_index.json"
+    library_path: Path = field(
+        default=Path.home() / ".llamabot/zotero/zotero_index.json"
+    )
 
     def __post_init__(self):
         """Post-initialization hook"""
-        self.library = [json.loads(line) for line in open(self.library_path, "r")]
+        library = [
+            ZoteroItem(json.loads(line), library=self)
+            for line in open(self.library_path, "r")
+        ]
+        self.library = {i["key"]: i for i in library}
 
     def __getitem__(self, key):
         """Get item by key.
@@ -22,7 +29,14 @@ class ZoteroLibrary:
         :param key: Key to get.
         :return: Value for key.
         """
-        return ZoteroItem([i for i in self.library if i["key"] == key][0], library=self)
+        return self.library[key]
+
+    def keys(self):
+        """Return all of the keys from the library.
+
+        :return: A list of keys.
+        """
+        return [i["key"] for i in self.library]
 
 
 @dataclass
@@ -30,13 +44,14 @@ class ZoteroItem:
     """Zotero item."""
 
     info: dict
-    library: ZoteroLibrary
+    library: Optional[ZoteroLibrary]
 
     def __getitem__(self, key):
         """Get item by key.
 
         :param key: Key to get.
         :return: Value for key.
+        :raises KeyError: If key is not found.
         """
         # Key should be a string that is dot-delimited.
         # we split the string into a list of keys
@@ -45,18 +60,57 @@ class ZoteroItem:
         keys = key.split(".")
         value = self.info
         for k in keys:
-            value = value[k]
+            try:
+                value = value[k]
+            except KeyError:
+                raise KeyError(f"Key {k} not found in {value}.")
         return value
+
+    def has_pdf(self):
+        """Check if this item has a PDF or not.
+
+        We check the "links" section, which typically looks like this:
+
+        ```json
+        {
+            'self': {
+                'href': 'https://api.zotero.org/users/5334442/items/K3WYABBQ',
+                'type': 'application/json'
+            },
+            'alternate': {
+                'href': 'https://www.zotero.org/ericmjl/items/K3WYABBQ',
+                'type': 'text/html'
+            },
+            'attachment': {
+                'href': 'https://api.zotero.org/users/5334442/items/U6X244QK',
+                'type': 'application/json',
+                'attachmentType': 'application/pdf',
+                'attachmentSize': 4351619
+            }
+        }
+        ```
+
+        :return: True if this item has a PDF, False otherwise.
+        """
+        if self.get("links.attachment.attachmentType") == "application/pdf":
+            return True
+        return False
+
+    def get(self, key_string, default_value=None):
+        try:
+            return self[key_string]
+        except KeyError:
+            return default_value
 
     def pdf(self):
         """Get the PDF entry for this item.
 
         :return: PDF entry.
+        :raises KeyError: If no PDF is found.
         """
-        pdf_url = self["links.attachment.href"]
-        pdf_key = pdf_url.split("/")[-1]
-        pdf_entry = self.library[pdf_key]
-        return pdf_entry
+        if self.has_pdf():
+            return self["links.attachment"]
+        raise KeyError("No PDF found.")
 
     def download_pdf(self, directory: Path) -> Path:
         """Download the PDF for this item.
@@ -64,9 +118,10 @@ class ZoteroItem:
         :param directory: Directory to download the PDF to.
         :return: Path to the downloaded PDF.
         """
-        pdf = self.pdf()
-        zot = load_zotero()
-        fpath = directory / f"{pdf['key']}.pdf"
-        with open(fpath, "wb") as f:
-            f.write(zot.file(pdf["key"]))
-        return fpath
+        if self.has_pdf():
+            pdf = self.pdf()
+            zot = load_zotero()
+            fpath = directory / f"{pdf['key']}.pdf"
+            with open(fpath, "wb") as f:
+                f.write(zot.file(pdf["key"]))
+            return fpath
