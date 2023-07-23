453e5ddd681eb89fb84c9f23a5a76dc2fafdf0ba
Eric Ma <ericmajinglong@gmail.com>
2023-07-22 21:29:05 -0400

    feat: enhance blog assistant CLI and blogging prompts
    This commit introduces several enhancements to the blog assistant CLI and blogging prompts. 
    
    - The `summarize_and_tag` function has been renamed to `summarize` and now also returns the blog title.
    - A new `social_media` function has been added to generate social media posts for LinkedIn, Patreon, and Twitter.
    - The `blog_tagger_and_summarizer` prompt has been renamed to `blog_title_tags_summary` and now also returns the blog title.
    - New prompts `compose_linkedin_post`, `compose_patreon_post`, and `compose_twitter_post` have been added to generate social media posts.
    - A new `BlogInformation` model has been added to represent blog information.
    
    These changes improve the functionality and usability of the blog assistant.

--- llamabot/cli/blog.py
+++ llamabot/cli/blog.py
@@ -1,7 +1,16 @@
 """Blog Assistant CLI"""
+
+import pyperclip
 import typer
 
-from llamabot.prompt_library.blog import blog_tagger_and_summarizer, blogging_bot
+from llamabot.prompt_library.blog import (
+    BlogInformation,
+    blog_title_tags_summary,
+    blogging_bot,
+    compose_linkedin_post,
+    compose_patreon_post,
+    compose_twitter_post,
+)
 from llamabot.prompt_library.output_formatter import coerce_dict
 
 from .utils import uniform_prompt
@@ -10,18 +19,51 @@ app = typer.Typer()
 
 
 @app.command()
-def summarize_and_tag():
-    """Summarize and tag a blog post."""
+def summarize():
+    """My standard blogging workflow."""
     bot = blogging_bot()
 
     typer.echo("Please paste your blog post below.")
     query = uniform_prompt()
-    response = bot(blog_tagger_and_summarizer(query)).content
+    response = bot(blog_title_tags_summary(query, BlogInformation)).content
     parsed_response = coerce_dict(response)
 
+    typer.echo("\n\n")
+    typer.echo("Here is your blog title:")
+    typer.echo(parsed_response["title"])
+
+    typer.echo("\n\n")
     typer.echo("Here is your blog summary:")
     typer.echo(parsed_response["summary"])
 
+    typer.echo("\n\n")
     typer.echo("Here are your blog tags:")
     for tag in parsed_response["tags"]:
         typer.echo(tag)
+
+
+@app.command()
+def social_media(platform: str):
+    """Generate social media posts.
+
+    :param platform: The social media platform to generate posts for.
+        Should be one of "linkedin", "patreon", or "twitter".
+    """
+    bot = blogging_bot()
+    typer.echo("Please paste your blog post below.")
+    query = uniform_prompt()
+
+    platform = platform.lower()
+
+    platform_to_compose_mapping = {
+        "linkedin": compose_linkedin_post,
+        "patreon": compose_patreon_post,
+        "twitter": compose_twitter_post,
+    }
+
+    compose_func = platform_to_compose_mapping[platform]
+
+    patreon_post = bot(compose_func(query)).content
+    pyperclip.copy(patreon_post)
+    typer.echo("\n\n")
+    typer.echo(f"Your {platform} post has been copied to your clipboard! 🎉")
--- llamabot/prompt_library/blog.py
+++ llamabot/prompt_library/blog.py
@@ -1,7 +1,16 @@
 """Blogging prompts."""
 from outlines import text
+from pydantic import BaseModel
 
 from llamabot import SimpleBot
+
+
+class BlogInformation(BaseModel):
+    """Blog information."""
+
+    tags: list[str]
+    summary: str
+    title: str
 
 
 def blogging_bot():
@@ -9,32 +18,88 @@ def blogging_bot():
 
     :return: The blogging bot.
     """
-    bot = SimpleBot("You are an expert blogger.")
+    bot = SimpleBot(
+        "You are an expert blogger. Whenever you use hashtags, they are always lowercase.",
+        temperature=0.3,
+    )
     return bot
 
 
 @text.prompt
-def blog_tagger_and_summarizer(blog_post):
+def blog_title_tags_summary(blog_post, blog_info_model):
     """This is a blog post that I just wrote.
 
     {{ blog_post }}
 
     Please return for me up to 15 blog tags in lowercase.
     They should be at most 2 words long.
+    Return one tag per line.
+    No symbols, just the words, all lowercase.
 
     Also, please return for me a summary of the blog post,
     written in first-person tone,
     that is at most 100 words long.
+    Use emojis where appropriate.
+
+    Finally, please suggest a title for the blog post.
     It should be entertaining without being overly so,
     and should entice readers to read the blog post.
-    Use emojis where appropriate.
 
     Respond with a JSON formatted as follows:
 
-    {
-        "tags": [<tag1>, <tag2>,...],
-        "summary": <summary>
-    }
+    {{ blog_info_model | schema }}
 
     # noqa: DAR101
+    """
+
+
+@text.prompt
+def compose_linkedin_post(blog_post):
+    """This is a blog post that I just wrote:
+
+    {{ blog_post }}
+
+    Please compose for me a LinkedIn post
+    that entices my network on LinkedIn to read it.
+    Ensure that there is a call to action to interact with the post after reading
+    to react with it, comment on it, or share the post with others,
+    and to support my work on Patreon.
+    My Patreon link is https://patreon.com/ericmjl/
+    Include hashtags inline with the LinkedIn post and at the end of the post too.
+
+    #noqa: DAR101
+    """
+
+
+@text.prompt
+def compose_patreon_post(blog_post):
+    """This is a blog post that I just wrote:
+
+    {{ blog_post }}
+
+    Please compose for me a Patreon post
+    that entices my patrons on Patreon to read it.
+    Ensure that there is a call to action to interact with the post after reading it,
+    such as asking a question, or suggesting ideas that I did not write about.
+    Please use emojis where appropriate as well!
+
+    #noqa: DAR101
+    """
+
+
+@text.prompt
+def compose_twitter_post(blog_post):
+    """This is a blog post that I just wrote:
+
+    {{ blog_post }}
+
+    Please compose for me a Twitter post
+    that entices my followers on Twitter to read it.
+    Ensure that there is a call to action to interact with the post after reading it,
+    such as retweeting, commenting, or sharing it with others,
+    and to support my work on Patreon.
+    My Patreon link is https://patreon.com/ericmjl/
+    Include hashtags inline with the Twitter post.
+
+    #noqa: DAR101
     """
