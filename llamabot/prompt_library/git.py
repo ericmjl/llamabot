ce91d6b83a2f9da4af1785d4edd9ea6d3c45b3ad
Eric Ma <ericmajinglong@gmail.com>
2023-09-10 14:13:34 -0400

    feat(git): update commitbot to use gpt-3.5-turbo-16k-0613 model
    It is hard-coded to use gpt-3.5-turbo-16k-0613 as its model.
    This model is sufficient for the quality of commit messages,
    matching gpt-4-32k but being 1/10 the cost.

--- llamabot/prompt_library/git.py
+++ llamabot/prompt_library/git.py
@@ -14,9 +14,15 @@ except ImportError:
 def commitbot():
     """Return a commitbot instance.
 
+    It is hard-coded to use gpt-3.5-turbo-16k-0613 as its model.
+    This model is sufficient for the quality of commit messages,
+    matching gpt-4-32k but being 1/10 the cost.
+
     :return: A commitbot instance.
     """
-    return SimpleBot("You are an expert user of Git.")
+    return SimpleBot(
+        "You are an expert user of Git.", model_name="gpt-3.5-turbo-16k-0613"
+    )
 
 
 @text.prompt
@@ -37,25 +43,30 @@ def write_commit_message(diff: str):
     [optional footer(s)]
     [COMMIT MESSAGE END]
 
-    The commit contains the following structural elements, to communicate intent to the consumers of your library:
+    The commit contains the following structural elements,
+    to communicate intent to the consumers of your library:
 
     fix: a commit of the type fix patches a bug in your codebase
         (this correlates with PATCH in Semantic Versioning).
     feat: a commit of the type feat introduces a new feature to the codebase
         (this correlates with MINOR in Semantic Versioning).
-    BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope,
-        introduces a breaking API change (correlating with MAJOR in Semantic Versioning).
+    BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:,
+        or appends a ! after the type/scope,
+        introduces a breaking API change
+        (correlating with MAJOR in Semantic Versioning).
         A BREAKING CHANGE can be part of commits of any type.
 
     types other than fix: and feat: are allowed,
-    for example @commitlint/config-conventional (based on the Angular convention) recommends
+    for example @commitlint/config-conventional
+    (based on the Angular convention) recommends
     build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
 
     footers other than BREAKING CHANGE: <description> may be provided
     and follow a convention similar to git trailer format.
 
     Additional types are not mandated by the Conventional Commits specification,
-    and have no implicit effect in Semantic Versioning (unless they include a BREAKING CHANGE).
+    and have no implicit effect in Semantic Versioning
+    (unless they include a BREAKING CHANGE).
     A scope may be provided to a commit's type,
     to provide additional contextual information and is contained within parenthesis,
     e.g., feat(parser): add ability to parse arrays.
