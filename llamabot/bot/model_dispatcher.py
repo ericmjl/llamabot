1b63365c13e8899b580a74f03f3832963990a4f8
Eric Ma <ericmajinglong@gmail.com>
2023-10-23 08:45:18 -0400

    feat: Add subprocess management to model_dispatcher
    This commit introduces a new feature to the model_dispatcher module in
    the llamabot bot. It adds the ability to manage subprocesses,
    specifically to launch the Ollama API locally and terminate it if the
    Python process dies. This is achieved through a new decorator function
    `kill_process_if_python_dies` and a new function `launch_ollama`.
    
    The `kill_process_if_python_dies` decorator function is designed to kill
    a subprocess if the Python process dies. It is used within llamabot to
    launch the Ollama API locally in an automagical way.
    
    The `launch_ollama` function is used to launch the Ollama API. It uses
    the `kill_process_if_python_dies` decorator to ensure that the API is
    terminated if the Python process dies.
    
    This commit also includes additional logging and signal handling to
    manage the subprocesses effectively.

--- llamabot/bot/model_dispatcher.py
+++ llamabot/bot/model_dispatcher.py
@@ -1,8 +1,17 @@
 """Dispatch to the appropriate model based on string name."""
 
+import subprocess
+import signal
+import sys
+from functools import wraps
+import os
+
+
 from langchain.chat_models import ChatOpenAI, ChatOllama
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.base import BaseCallbackManager
+from time import sleep
+from loguru import logger
 
 # get this list from: https://ollama.ai/library
 ollama_model_keywords = [
@@ -56,6 +65,7 @@ def create_model(
     ModelClass = ChatOpenAI
     if model_name.split(":")[0] in ollama_model_keywords:
         ModelClass = ChatOllama
+        launch_ollama(model_name, verbose=verbose)
 
     return ModelClass(
         model_name=model_name,
@@ -66,3 +76,88 @@ def create_model(
             handlers=[StreamingStdOutCallbackHandler()] if streaming else []
         ),
     )
+
+
+def kill_process_if_python_dies(command_template):
+    """Decorator to kill a subprocess if the Python process dies.
+
+    NOTE: This function is challenging to test.
+    I am not sure how to test it at the moment.
+
+    This decorator is used within llamabot
+    to launch the Ollama API locally in an automagical way.
+    However, it definitely is not specific to llamabot
+    and can probably be used in other settings.
+
+    :param command_template: The command template to use.
+    """
+    child_process = None
+
+    def signal_handler(signal_number, frame):
+        """Signal handler for SIGINT and SIGTERM.
+
+        This function terminates the child process and exits the program
+        if the child process is running.
+
+        :param signal_number: The signal number.
+            In plain English, this is the signal
+            that was received from the operating system,
+            which is usually a string like 'SIGINT' or 'SIGTERM'.
+        :param frame: The frame.
+            In plain English, this is the execution frame
+            that was interrupted by the signal,
+            which is usually the main thread.
+        """
+        nonlocal child_process
+        if child_process:
+            logger.info("Terminating child process")
+            child_process.terminate()
+        sys.exit(0)
+
+    def decorator(func):
+        """Decorator to manage the child process.
+
+        :param func: The function to decorate.
+        """
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            """Wrapper function to manage the child process.
+
+            :param args: The positional arguments.
+            :param kwargs: The keyword arguments.
+            """
+            nonlocal child_process
+            signal.signal(signal.SIGINT, signal_handler)
+            signal.signal(signal.SIGTERM, signal_handler)
+
+            model_name = args[0] if args else kwargs.get("model_name", "")
+            command = [part.format(model_name=model_name) for part in command_template]
+
+            verbose = kwargs.get("verbose", True)
+            if verbose:
+                child_process = subprocess.Popen(command)
+            else:
+                with open(os.devnull, "w") as nullfile:
+                    child_process = subprocess.Popen(
+                        command, stdout=nullfile, stderr=nullfile
+                    )
+            logger.info(f"Launched child process with PID {child_process.pid}")
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+# Use a template for the command, with {model_name} as a placeholder for the model name
+@kill_process_if_python_dies(["ollama", "serve", "{model_name}"])
+def launch_ollama(model_name, verbose=True):
+    """Launch the Ollama API.
+
+    :param model_name: The name of the model to use.
+    :param verbose: Whether to print debug messages.
+    """
+    logger.info("Launching Ollama API...")
+    sleep(2)
