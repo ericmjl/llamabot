ee7eb83e48382050a20b4fc049a7a029f33b6506
Eric Ma <ericmajinglong@gmail.com>
2023-12-23 15:44:07 -0500

    refactor(model_tokens): rename and update model token budgets
    - Renamed `model_chat_token_budgets` to `model_context_window_sizes` to better reflect its purpose.
    - Updated the token budgets for various models, including the addition of new `mistral` model variants.

--- llamabot/bot/model_tokens.py
+++ llamabot/bot/model_tokens.py
@@ -4,7 +4,7 @@ DEFAULT_TOKEN_BUDGET = 4_096
 # Reference: https://platform.openai.com/docs/models
 # For the Ollama models, refer to individual model documentation page: https://ollama.ai/library
 # Please update the numbers here as necessary.
-model_chat_token_budgets = {
+model_context_window_sizes = {
     "gpt-3.5-turbo-0301": 4_097,
     "gpt-3.5-turbo-0613": 4_097,
     "gpt-3.5-turbo-16k-0613": 16_385,
@@ -19,5 +19,7 @@ model_chat_token_budgets = {
     "gpt-4": 8_192,
     "llama2": 4_096,
     "vicuna:7b-16k": 16_385,
-    "mistral": 8_192,
+    "mistral/mistral-tiny": 4_096,
+    "mistral/mistral-small": 8_192,
+    "mistral/mistral-medium": 32_768,
 }
