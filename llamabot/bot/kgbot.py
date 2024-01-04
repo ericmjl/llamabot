963cd6303943a895596eef3ca4b39a04828bb9d2
Eric Ma <ericmajinglong@gmail.com>
2024-01-04 11:56:26 -0500

    feat: Add Knowledge Graph bot
    This commit introduces a new feature to the codebase, the Knowledge Graph bot (KGBot). The KGBot takes in a chunk of text and returns a JSON of triplets. It is tested with mistral-medium and uses the default language model. The bot is called with a query and returns a JSON of triplets.

---
+++ llamabot/bot/kgbot.py
@@ -0,0 +1,44 @@
+"""Knowledge Graph bot."""
+import json
+from llamabot.components.messages import HumanMessage
+from llamabot.config import default_language_model
+
+from llamabot.prompt_manager import prompt
+from .simplebot import SimpleBot
+
+
+@prompt
+def kgbot_sysprompt() -> str:
+    """You are an expert ontologist. You are tasked with taking in a chunk of text
+    and extrapolating as many relationships as possible from that text.
+
+    For each relationship, return a JSON according to the following schema:
+
+    {
+        "subject": "string",
+        "predicate": "string",
+        "object": "string"
+    }
+    """
+
+
+class KGBot(SimpleBot):
+    """KGBot is the Knowledge Graph bot.
+
+    Tested with mistral-medium.
+
+    It takes in a chunk of text and returns a JSON of triplets.
+    """
+
+    def __init__(self):
+        SimpleBot.__init__(
+            self, system_prompt=kgbot_sysprompt(), model_name=default_language_model()
+        )
+
+    def __call__(self, query: str) -> dict:
+        """Call the bot with a query and return a JSON of triplets.
+
+        :param query: The query to use.
+        """
+        response = self.generate_response([HumanMessage(content=query)])
+        return json.loads(response.content)
