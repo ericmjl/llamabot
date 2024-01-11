21e0bfc503571b1b359cfa9bb4a853bca55a659f
Eric Ma <ericmajinglong@gmail.com>
2024-01-10 23:06:24 -0500

    fix(llamabot): update logging of API key
    - Updated the logging message in SimpleBot to only display the first 5 characters of the API key for security reasons.
      Also, added the ability to retrieve the API key from the environment variable MISTRAL_API_KEY in the notebook.

--- llamabot/bot/simplebot.py
+++ llamabot/bot/simplebot.py
@@ -79,7 +79,7 @@ class SimpleBot:
             completion_kwargs["response_format"] = {"type": "json_object"}
         if self.api_key:
             completion_kwargs["api_key"] = self.api_key
-            logger.info(f"Using API key from SimpleBot: {self.api_key[0:5]}")
+            logger.info(f"Using API key: {self.api_key[0:5]}...")
         response = completion(**completion_kwargs)
 
         if self.stream:
