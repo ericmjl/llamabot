37182cbc89b0cc2aeacad2e2719f1799d4aebe56
Eric Ma <ericmajinglong@gmail.com>
2024-01-20 23:23:12 -0500

    refactor(llamabot): remove unnecessary logger import and usage
    - Removed the import statement for `loguru.logger` which was not used elsewhere in the `simplebot.py`.
    - Eliminated the logging of the API key in `SimpleBot` class to enhance security and avoid potential leakage of sensitive information in logs.

--- llamabot/bot/simplebot.py
+++ llamabot/bot/simplebot.py
@@ -12,7 +12,6 @@ from llamabot.components.messages import (
 from llamabot.recorder import autorecord
 from llamabot.config import default_language_model
 from litellm import completion
-from loguru import logger
 
 prompt_recorder_var = contextvars.ContextVar("prompt_recorder")
 
@@ -77,7 +76,6 @@ class SimpleBot:
             completion_kwargs["response_format"] = {"type": "json_object"}
         if self.api_key:
             completion_kwargs["api_key"] = self.api_key
-        logger.info(f"Using SimpleBot API key: {self.api_key}...")
         response = completion(**completion_kwargs)
 
         if self.stream:
