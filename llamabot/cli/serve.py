69f42587f7f7d3a0549be6e4e803d65424ed2c13
Mark Harrison <mark@tigereye.com>
2024-05-26 20:51:52 -0700

    Add /api/tag support.

--- llamabot/cli/serve.py
+++ llamabot/cli/serve.py
@@ -133,6 +133,59 @@ async def api_generate(request: fastapi.Request):
     )
 
 
+# -------------------------------------------------------------------------------
+# /api/tags
+
+
+class TagsModelDetail(pydantic.BaseModel):
+    format: str
+    family: str
+    families: Optional[None] = None
+    parameter_size: str
+    quantization_level: str
+
+
+class TagsModel(pydantic.BaseModel):
+    name: str
+    modified_at: str
+    size: int
+    digest: str
+    details: TagsModelDetail
+
+
+class TagsRootModel(pydantic.BaseModel):
+    models: List[TagsModel]
+
+
+# Example usage
+data = {
+    "models": [
+        {
+            "name": "Placeholder",
+            "modified_at": "2023-11-04T14:56:49.277302595-07:00",
+            "size": 7365960935,
+            "digest": "9f438cb9ffffffc025612d27f7c1a6669ff83a8bb0ed86c94fcf4c5440555697",
+            "details": {
+                "format": "gguf",
+                "family": "placeholder",
+                "families": None,
+                "parameter_size": "13B",
+                "quantization_level": "Q4_0",
+            },
+        },
+    ]
+}
+
+
+@app.post("/api/tags")
+async def get_tags():
+    return TagsRootModel(**data)
+
+
+# -------------------------------------------------------------------------------
+# cli interface
+
+
 @cli.command()
 def querybot(
     system_prompt: str = typer.Option(..., help="System prompt."),
