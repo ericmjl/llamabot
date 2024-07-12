03d4c2bd8d5cd7bdfb3ba60313012e889e89fcc3
Eric Ma <ericmajinglong@gmail.com>
2024-07-12 15:53:32 -0700

    refactor(scipy2024/pylab_example): switch from pylab to matplotlib and numpy
    - Replaced `from pylab import *` with explicit imports of `matplotlib.pyplot` and `numpy` to improve readability and reduce namespace pollution.
    - Updated random number generation to use `numpy.random.random` for clarity and consistency.
    - Modified plotting commands to use the `matplotlib.pyplot` interface directly, enhancing code maintainability.

--- talks/scipy2024/pylab_example.py
+++ talks/scipy2024/pylab_example.py
@@ -1,10 +1,12 @@
-from pylab import *
+"""git commit diff example."""
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = rand(20)
-y = rand(20)
+x = np.random.random(20)
+y = np.random.random(20)
 
 # Lines on top of scatter
-figure()
-subplot(211)
-plot(x, y, "r", lw=3)
-scatter(x, y, s=120)
+plt.figure()
+plt.subplot(211)
+plt.plot(x, y, "r", lw=3)
+plt.scatter(x, y, s=120)
