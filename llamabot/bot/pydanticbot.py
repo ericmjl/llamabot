81baa753efe1856ab16bdfa7bccad7e296a8df60
Elliot <elliot.sal@gmail.com>
2024-07-15 21:57:03 -0700

    pydantic bot now inherits from simplebot, and instead internally keeps track of messages, response and unvalidated prompts back. it also handles adding the schema to the system prompt for you. This simplifies the interface a bit.

--- llamabot/bot/pydanticbot.py
+++ llamabot/bot/pydanticbot.py
@@ -1,13 +1,13 @@
 import contextvars
 
-from llamabot import ChatBot
+from llamabot import SimpleBot
 from llamabot.components.messages import (
-    AIMessage,
+    AIMessage, HumanMessage, SystemMessage,
 )
 from pydantic import BaseModel, ValidationError
 
 
-class PydanticBot(ChatBot):
+class PydanticBot(SimpleBot):
     """Pydantic Bot is given a Pydantic Model and expects the LLM to return a JSON structure that conforms to the model schema
     It will validate the model, repeating the prompt if it does not validate, and then returns an instance of that model.
     """
@@ -15,17 +15,27 @@ class PydanticBot(ChatBot):
     def __init__(
         self,
         system_prompt: str,
-        session_name: str,
         pydantic_model: BaseModel,
+        stream_target: str = "stdout",
         num_attempts: int = 3,
-        **kwargs,
+        **completion_kwargs,
     ):
-        super().__init__(system_prompt, session_name, json_mode=True, **kwargs)
+        if stream_target not in ("stdout",):
+            raise ValueError(
+                f"stream_target must be one of ('stdout'), got {stream_target}."
+            )
+
+        super().__init__(system_prompt, stream_target=stream_target, json_mode=True, **completion_kwargs)
+
         self.pydantic_model = pydantic_model
         self.num_attempts = num_attempts
 
-    def get_validation_error_prompt(self, exception):
-        return f"There was a validation error: {exception.json()} try again."
+    def get_system_message(self) -> SystemMessage:
+        schema = self.pydantic_model.schema_json()
+        return SystemMessage(content=self.system_prompt.content + f"\n\nYour task is to return the data in a json object that matches the following json_schema:\n```{ schema }```\nOnly return an INSTANCE of the schema, do not return the schema itself.")
+
+    def get_validation_error_message(self, exception) -> HumanMessage:
+        return HumanMessage(content=f"There was a validation error: {exception.json()} try again.")
 
     def _extract_json_from_response(self, response:AIMessage):
         content = response.content
@@ -34,17 +44,24 @@ class PydanticBot(ChatBot):
         return content[first_paren: last_paren + 1]
 
     def __call__(self, message: str) -> BaseModel:
-        prompt = message
+        messages = [self.get_system_message(), HumanMessage(content=message)]
+
+        # we'll attempt to get the response from the model and validate it
         for attempt in range(self.num_attempts):
             try:
-                response = ChatBot.__call__(self, prompt)
+                response = self.stream_stdout(messages)
 
                 # parse the response, and validate it against the pydantic model
                 codeblock = self._extract_json_from_response(response)
                 obj = self.pydantic_model.model_validate_json(codeblock)
                 return obj
             except ValidationError as e:
+                # we've exceeded the maximum number of attempts, we raise the error the higher
                 if attempt >= self.num_attempts - 1:
                     raise e
 
-                prompt = self.get_validation_error_prompt(e)
+                # if we failed, we give the model the validation error, and try again
+                messages.extend([
+                    response,
+                    self.get_validation_error_message(e)
+                ])
