178739623cadc08349e1e0c23f325b550119e874
Eric Ma <ericmajinglong@gmail.com>
2025-02-05 08:33:33 -0500

    refactor(ci)🔧: Refactor test scripts in CI workflows for llamabot.
    - Replaced inline test commands with a dedicated script for llamabot testing.
    - Added a new script, yo.py, to encapsulate llamabot test logic.
    - Updated CI workflow to utilize the new script for testing.

---
+++ scripts/yo.py
@@ -0,0 +1,22 @@
+"""A simple script to test the llamabot package."""
+
+import llamabot as lmb
+from pydantic import BaseModel
+
+bot = lmb.SimpleBot("yo", model_name="ollama_chat:gemma2:2b")
+
+print(bot("sup?"))
+
+
+class Person(BaseModel):
+    """A person."""
+
+    name: str
+    age: int
+
+
+sbot = lmb.StructuredBot(
+    "yo", model_name="ollama_chat:gemma2:2b", pydantic_model=Person
+)
+
+print(sbot("what is your name and age?"))
